bc30c0ee1bcc9d3bf7350b3b6da61cea52636bcb
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2023-11-13 18:46:18 +0100

    Display li elements of array items using ngFor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  //templateUrl: './app.component.html',
-  template: `<body>
-    <h1>Welcome to {{ title }}!</h1>
-    <app-input-button-unit> </app-input-button-unit>
-  </body>`,
+  templateUrl: './app.component.html',
+ 
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
-  ley = [1, '23 ', 'qwe dwe', { r: 18, s: 'nastya' }];
+  todoList = [
+    { title: 'install Node JS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
 }
