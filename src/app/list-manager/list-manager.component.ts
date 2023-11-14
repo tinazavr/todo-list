f3ac1486cd176d322a62e222c85785780fcb21c2
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2023-11-14 19:56:22 +0100

    Created a service which will managing all the tasks, and  injected it into the list-manager component.

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import {TodoListService} from '../services/todo-list.service';
 
 @Component({
   selector: 'app-list-manager',
@@ -7,16 +8,13 @@ import { TodoItem } from '../interfaces/todo-item';
   styleUrls: ['./list-manager.component.scss'],
 })
 export class ListManagerComponent implements OnInit {
-  todoList: TodoItem[] = [
-    { title: 'install Node JS' },
-    { title: 'install Angular CLI' },
-    { title: 'create new app' },
-    { title: 'serve app' },
-    { title: 'develop app' },
-    { title: 'deploy app' },
-  ];
-  constructor() {}
-  ngOnInit(): void {}
+  todoList: TodoItem[];
+
+  constructor(private todoListService: TodoListService) {}
+  
+  ngOnInit() {
+    this.todoList = this.todoListService.getTodoList();
+  }
 
   addItem(title: string) {
     this.todoList.push({ title: title });
